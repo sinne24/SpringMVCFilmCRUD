a6c9e8fd6136d593fa25bc4780c2ac5934eeb39c
Cindy <cindy@Students-MacBook-Pro.local>
2021-09-11 12:12:12 -0600

    Modified the controller and created result.jsp

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -3,17 +3,34 @@ package com.skilldistillery.film.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.dao.FilmDaoJdbcImpl;
 
 @Controller 
 public class FilmController {
 	
 	@Autowired 
-	private FilmDAO filmDao;
+	private FilmDaoJdbcImpl filmDao;
 	
 	@RequestMapping( path = {"/","home.do"} )
 	public String home() {
 		return"WEB-INF/home.jsp";
+	}
+
+	//@RequestMapping(path = "GetStateData.do", params = "name", method = RequestMethod.GET)
+	//public ModelAndView stateMethod(@RequestParam("name") String n) {
+	
+	@RequestMapping( path = {"retrieveFilm.do"}, params = "id", method = RequestMethod.GET)
+	public ModelAndView retrieveFilm(@RequestParam("id") String id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("WEB-INF/result.jsp");
+
+		int filmID = Integer.parseInt(id);
+		mv.addObject("film", filmDao.findFilmById(filmID));
+		
+		return mv;
 	}
 }
