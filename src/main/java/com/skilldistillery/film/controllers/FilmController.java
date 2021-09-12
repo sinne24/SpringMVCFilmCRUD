306f2cce670ffeefc38afc5a662c24cec1701f3d
ronald-viramontes <ronald.viramontes@gmail.com>
2021-09-12 16:39:58 -0400

    edit filmController

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.film.dao.FilmDaoJdbcImpl;
 import com.skilldistillery.film.entities.Film;
@@ -21,38 +22,32 @@ public class FilmController {
 		return "WEB-INF/home.jsp";
 	}
 
-	// @RequestMapping(path = "GetStateData.do", params = "name", method =
-	// RequestMethod.GET)
-	// public ModelAndView stateMethod(@RequestParam("name") String n) {
-
 	@RequestMapping(path = { "retrieveFilm.do" }, params = "id", method = RequestMethod.GET)
 	public ModelAndView retrieveFilm(@RequestParam("id") String id) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("WEB-INF/result.jsp");
-
 		int filmID = Integer.parseInt(id);
 		mv.addObject("film", filmDao.findFilmById(filmID));
 
 		return mv;
-
 	}
 
-	@RequestMapping(path = "createFilm.do",
-            params = "film",
-            method = RequestMethod.POST)
-public ModelAndView createFilm(String title, String description, Integer releaseYear, int languageId, int rentalDuration,
-		double rentalRate, Integer length, double replacementCost, String rating, String specialFeatures){
-ModelAndView mv = new ModelAndView();
-mv.setViewName("redirect:confirmation.do");
-mv.addObject("film", filmDao.createFilm(title, description, releaseYear, languageId, rentalDuration,
-				rentalRate, length, replacementCost, rating, specialFeatures));
-      return mv;
-}
+	@RequestMapping(path = "createFilm.do", params = "film", method = RequestMethod.POST)
+	public ModelAndView createFilm(String title, String description, Integer releaseYear, int languageId,
+			int rentalDuration, double rentalRate, Integer length, double replacementCost, String rating,
+			String specialFeatures, RedirectAttributes redir) {
+		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute("film", filmDao.createFilm(title, description, releaseYear, languageId, rentalDuration, rentalRate,
+				length, replacementCost, rating, specialFeatures));
+		mv.setViewName("redirect:confirmation.do");
+		
+		return mv;
+	}
 
 	@RequestMapping(path = "confirmation.do", method = RequestMethod.GET)
 	public ModelAndView filmCreated() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("WEB-INF/confirmation.jsp");
+		mv.setViewName("WEB-INF/result.jsp");
 		return mv;
 	}
 
@@ -64,18 +59,18 @@ mv.addObject("film", filmDao.createFilm(title, description, releaseYear, languag
 	}
 
 	@RequestMapping(path = "deleteFilm.do", params = "id", method = RequestMethod.POST)
-	public ModelAndView deleteFilm(int filmId) {
+	public ModelAndView deleteFilm(int filmId, RedirectAttributes redir) {
 		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute(filmDao.deleteFilm(filmId));
 		mv.setViewName("redirect:actionComplete.do");
-		mv.addObject(filmDao.deleteFilm(filmId));
 		return mv;
 	}
 
 	@RequestMapping(path = "editFilm.do", params = "film", method = RequestMethod.POST)
-	public ModelAndView editFilm(Film film) {
+	public ModelAndView editFilm(Film film, RedirectAttributes redir) {
 		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute(filmDao.editFilm(film));
 		mv.setViewName("redirect:confirmation.do");
-		mv.addObject(filmDao.editFilm(film));
 		return mv;
 	}
 
